4d77c29de8107c78aece29b58840228cd24b753d
ekahanny <ekahnny@gmail.com>
2024-09-23 15:12:01 +0800

    membuat route

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
 
-// mendefinisikan request apa yang dijalankan
-app.use(() => {
-  console.log("Hello World!");
+// mendefinisikan request apa yang dijalankan serta respon apa yg diterima
+// app.use((req, res) => {
+//   console.log("Hello World!");
+//   res.send('<h1>Hello World!</h1>')
+// });
+
+//routing
+app.get("/", (req, res) => {
+  res.send("This is homepage");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("this is cats page");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("this is cats page from post");
 });
 
 // menjalankan server
 app.listen(8080, () => {
   console.log("server is running on http://localhost:8080");
+});
+
+// membuat handling page untuk route yang tidak terdaftar
+app.get("*", (req, res) => {
+  res.send("Halaman tidak ditemukan");
 });
