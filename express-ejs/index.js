cb4d7afe0dceea0034bc25892ca8de61777dc164
ekahanny <ekahnny@gmail.com>
2024-11-05 11:43:29 +0800

    demo passing data

--- express-ejs/index.js
+++ express-ejs/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const path = require("path");
 
 app.set("view engine", "ejs");
 
-// mengatur posis folder views
+// mengatur posisi folder views
 app.set("views", path.join(__dirname, "/views"));
 
 // route
@@ -16,6 +17,11 @@ app.get("/rand", (req, res) => {
   const num = Math.floor(Math.random() * 10) + 1;
   // passing data ke template, di template random, panggil variabel bernama random
   res.render("random", { random: num });
+});
+
+app.get("/t/:tag", (req, res) => {
+  const { tag } = req.params;
+  res.render("tag", { tag });
 });
 
 app.listen(8080, () => {
